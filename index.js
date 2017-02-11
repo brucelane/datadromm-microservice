ef5d2cb45f5be3bf4778ba64be76cb25a198d0b8
Josep <josep.arpi@gmail.com>
2017-02-11 19:33:57 +0100

    Server listen and persistence setup
    Mongodb connection + promisfy init
    Added json config file to easily manage app configuration

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var config = require('./config/config.js');
+var config = require('./config/config.json');
 var app = express();
 
 var port = process.env.port || 8080;
@@ -9,6 +9,32 @@ app.use(bodyParser.urlencoded( {extended: false} ));
 app.use(bodyParser.json());
 if(config.environment === "dev") app.use(morgan('dev'));
 
-app.listen(port, function(){
-    console.log('app initialized');
-});
+function initHttp() {
+    return new Promise(function(resolve, reject){
+        app.listen(port, function(err){
+            if (err) return reject(err);
+            return resolve(true);
+        });
+    });
+}
+
+function initDB() {
+    return new Promise(function(resolve, reject){
+        mongoose.connect(config.db_host, function(err){
+            if (err) return reject(err);
+            return resolve(true);
+        });
+    });
+}
+
+initHttp()
+    .then(function(){
+        console.log('app initialized on port ' + port);
+    })
+    .then(initDB)
+    .then(function(){
+        console.log('db connected');
+    })
+    .catch(function(err){
+        console.dir(err);
+    });
