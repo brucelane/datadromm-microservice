5d1ebdce143254049eb4a8aa137b21cd480b1e67
Josep <josep.arpi@gmail.com>
2017-02-11 19:44:06 +0100

    Gulpfile Add task to restart server on file changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,9 @@
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
 
+gulp.task('debug', function () {
+    nodemon({
+            script: 'index.js'
+          , env: { 'NODE_ENV': 'development'  }
+    });
+});
