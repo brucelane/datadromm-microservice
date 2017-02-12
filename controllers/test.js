41e0a4666942af3b2357051cb3e6c219728187bd
Bruce LANE <bruce@batchass.fr>
2017-02-12 12:05:33 +0100

    to fix: DELETE

---
+++ controllers/test.js
@@ -0,0 +1,54 @@
+var Shader = require('../models/test.js');
+
+function addShader() {
+    return new Promise(function(resolve, reject) {
+        // create shader
+        var shader = new Shader({
+                name: "flyingSaucers",
+                text: "void main(void) {gl_FragColor = vec4(1.0,1.0,0.0,1.0);}",
+                url: "https://www.shadertoy.com/view/Md23DV",
+                created: new Date(),
+                isValid : false 
+        }); 
+        // save to mongodb
+        shader.save(function (err, shaderInserted)
+        {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(shaderInserted);
+            
+        });
+    });
+}
+function listShaders() {
+    return new Promise(function(resolve, reject) {
+        Shader.find(function(err, shaderList) {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(shaderList);
+        });       
+    });
+}
+function removeShader(shaderId) {
+    return new Promise(function(resolve, reject) {
+       Shader.findOneAndRemove({id: shaderId}).exec( 
+            function(err, shaderToDelete) {
+            console.log(shaderId);
+            console.log(shaderToDelete);
+            if (err) {
+                console.log(err);
+                return reject(err);
+            }
+            console.log("ok deleted");
+            return resolve(shaderToDelete);
+        
+        });
+    });
+}
+module.exports = {
+    createShader: addShader,
+    getShaderList: listShaders,
+    deleteShader: removeShader
+};
--- routes/test.js
+++ routes/test.js
@@ -1,11 +1,22 @@
-var ShaderController = require('../controllers/index.js');
+var ShaderController = require('../controllers/test.js');
 
 function defaultRoute(app) {
 
     app.get('/', function(req, res) {
         res.status(200).json('OK');
     });
-    
+    app.get('/shaders', function(req, res) {
+        ShaderController.getShaderList()
+        .then(function(shaderList) {
+            res.status(200).json(shaderList);
+        })
+    });
+    app.delete('/shader/:id', function(req, res) {
+        ShaderController.deleteShader(req.params.id)
+        .then(function() {
+            res.status(200).json('Deleted');
+        });
+    });
     app.post('/shaders', function(req, res) {
         ShaderController.createShader()
         .then(function(shaderInserted) {
